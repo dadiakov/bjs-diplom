de3ef20531b475431583f2c70f2c8b17e87da275
Dmitry Diakov <d.diakov@list.ru>
2021-03-24 19:58:27 +0500

    DRY revision

--- public/homePage.js
+++ public/homePage.js
@@ -22,31 +22,44 @@ getCurrentVault();
 setInterval(getCurrentVault, 60000);
 
 const moneyManager = new MoneyManager();
+
+function moneyManagerSub(response, successMessage) {
+    response.success && ProfileWidget.showProfile(response.data);
+    let message = '';
+    moneyManager.setMessage(response.success, message = response.success ? successMessage : response.error);
+};
+
 moneyManager.addMoneyCallback = (data) => {
     ApiConnector.addMoney(data, response => {
-        response.success && ProfileWidget.showProfile(response.data);
-        let message = '';
-        moneyManager.setMessage(response.success, message = response.success ? 'Пополнение прошло успешно!': response.error);
+        moneyManagerSub(response, 'Пополнение прошло успешно!');
     });
 };
 
 moneyManager.conversionMoneyCallback = (data) => {
     ApiConnector.convertMoney(data, response => {
-        response.success && ProfileWidget.showProfile(response.data);
-        let message = '';
-        moneyManager.setMessage(response.success, message = response.success ? 'Конвертация прошла успешно!': response.error);
+        moneyManagerSub(response, 'Конвертация прошла успешно!');
     });
 };
 
 moneyManager.sendMoneyCallback = (data) => {
     ApiConnector.transferMoney(data, response => {
-        response.success && ProfileWidget.showProfile(response.data);
-        let message = '';
-        moneyManager.setMessage(response.success, message = response.success ? 'Перевод прошел успешно!': response.error);
+        moneyManagerSub(response, 'Перевод прошел успешно!');
     });
 };
 
 const favoritesWidget = new FavoritesWidget();
+
+function favoritesWidgetSub(response, successMessage) {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      favoritesWidget.setMessage(response.success, successMessage);
+        } else {
+          favoritesWidget.setMessage(response.success, response.error);
+        }
+};
+
 ApiConnector.getFavorites(response => {
     response.success && favoritesWidget.clearTable();
     favoritesWidget.fillTable(response.data);
@@ -55,26 +68,12 @@ ApiConnector.getFavorites(response => {
 
 favoritesWidget.addUserCallback = (data) => {
     ApiConnector.addUserToFavorites(data, response => {
-        if (response.success) {
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(response.data);
-            moneyManager.updateUsersList(response.data);
-            favoritesWidget.setMessage(response.success, 'Добавление прошло успешно!');
-        } else {
-            favoritesWidget.setMessage(response.success, response.error);
-        }
+        favoritesWidgetSub(response, 'Добавление прошло успешно!');
     });
-}
+};
 
 favoritesWidget.removeUserCallback = (data) => {
     ApiConnector.removeUserFromFavorites(data, response => {
-        if (response.success) {
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(response.data);
-            moneyManager.updateUsersList(response.data);
-            favoritesWidget.setMessage(response.success, 'Удаление прошло успешно!');
-        } else {
-            favoritesWidget.setMessage(response.success, response.error);
-        }
+        favoritesWidgetSub(response, 'Удаление прошло успешно!');
     });
-}
+};
